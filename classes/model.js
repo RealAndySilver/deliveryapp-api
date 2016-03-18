1f5a351bbccd936b24b4c685a7de7bb1273180de
Julian Montana <julian@apriorins.com>
2016-03-18 09:03:05 -0500

    Removing franchise from create payment method

--- classes/model.js
+++ classes/model.js
@@ -3233,7 +3233,7 @@ exports.createPaymentMethod = function(req,res){
 				user_id : req.body.user_id,
 				token : token,
 				card_last4 : req.body.card_number.substr(req.body.card_number.length-4, req.body.card_number.length),
-				franchise : req.body.franchise,
+				franchise : payments.getFranchiseByBIN(req.body.card_number),
 				date_created: new Date(),
 			}).save(function(err,object){
 				if(err){
@@ -3278,49 +3278,7 @@ exports.deletePaymentMethod = function(req,res){
 
 
 exports.getFranchiseByBIN = function (req,res){
-    var franchise="";
-    var number=req.params.bin?req.params.bin:"";
-
-    //VISA
-    var re = new RegExp("^4");
-    if (number.match(re) != null)
-        franchise= "VI";
-
-    // Mastercard
-    re = new RegExp("^5[1-5]");
-    if (number.match(re) != null)
-        franchise= "MC";
-
-    // AMEX
-    re = new RegExp("^3[47]");
-    if (number.match(re) != null)
-        franchise= "AM";
-
-    re = new RegExp("^36");
-    if (number.match(re) != null)
-        franchise= "DI";
-
-    // Visa Electron
-    re = new RegExp("^(4026|417500|4508|4844|491(3|7))");
-    if (number.match(re) != null)
-        franchise=  "VE";
-
-    // Discover
-    /*re = new RegExp("^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)");
-    if (number.match(re) != null)
-        franchise= "Discover";*/
-
-    // Diners
-        // Diners - Carte Blanche
-    /*re = new RegExp("^30[0-5]");
-    if (number.match(re) != null)
-        franchise= "Diners - Carte Blanche";*/
-
-    // JCB
-    /*re = new RegExp("^35(2[89]|[3-8][0-9])");
-    if (number.match(re) != null)
-        franchise= "JCB";*/
-
+    var franchise=payments.getFranchiseByBIN(req.params.bin?req.params.bin:"");
     res.json({status: false, response: franchise});
 };
 
--- classes/payments.js
+++ classes/payments.js
@@ -51,3 +51,50 @@ exports.deleteToken = function(token){
 
 	return true;
 };
+
+
+exports.getFranchiseByBIN = function (number){
+    var franchise="";
+
+    //VISA
+    var re = new RegExp("^4");
+    if (number.match(re) != null)
+        franchise= "VI";
+
+    // Mastercard
+    re = new RegExp("^5[1-5]");
+    if (number.match(re) != null)
+        franchise= "MC";
+
+    // AMEX
+    re = new RegExp("^3[47]");
+    if (number.match(re) != null)
+        franchise= "AM";
+
+    re = new RegExp("^36");
+    if (number.match(re) != null)
+        franchise= "DI";
+
+    // Visa Electron
+    re = new RegExp("^(4026|417500|4508|4844|491(3|7))");
+    if (number.match(re) != null)
+        franchise=  "VE";
+
+    // Discover
+    /*re = new RegExp("^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)");
+    if (number.match(re) != null)
+        franchise= "Discover";*/
+
+    // Diners
+        // Diners - Carte Blanche
+    /*re = new RegExp("^30[0-5]");
+    if (number.match(re) != null)
+        franchise= "Diners - Carte Blanche";*/
+
+    // JCB
+    /*re = new RegExp("^35(2[89]|[3-8][0-9])");
+    if (number.match(re) != null)
+        franchise= "JCB";*/
+
+    return franchise;
+};
